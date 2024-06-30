525debf5953a29530a9fba1f9112b6d94c7bd018
victorien <victorienmichel3@gmail.com>
2024-07-01 01:05:01 +0200

    CHORE: add function docstrings

--- mlfoil/scrap.py
+++ mlfoil/scrap.py
@@ -7,6 +7,19 @@ from bs4 import BeautifulSoup
 
 
 def get_polars(airfoil_name, reynolds):
+    """
+    Retrieves polar data for a given airfoil and Reynolds number from airfoiltools.com.
+
+    Args:
+        airfoil_name (str): The name of the airfoil.
+        reynolds (int): The Reynolds number.
+
+    Returns:
+        list: A list of lists representing the polar data, where each inner list contains the values for a specific angle of attack.
+
+    Raises:
+        Exception: If the request to airfoiltools.com fails or if the HTML parsing fails.
+    """
     url = f"http://airfoiltools.com/polar/details?polar=xf-{airfoil_name}-{reynolds}"
     response = requests.get(url)
     soup = BeautifulSoup(
@@ -19,6 +32,19 @@ def get_polars(airfoil_name, reynolds):
 
 
 def get_coords(airfoil_name):
+    """
+    Retrieves the coordinates of an airfoil from the Airfoil Tools website.
+
+    Parameters:
+    - airfoil_name (str): The name of the airfoil.
+
+    Returns:
+    - list: A list of coordinate pairs representing the airfoil's shape.
+
+    Example:
+    >>> get_coords("NACA0012")
+    [[1.000, 0.000], [0.998, 0.024], [0.993, 0.048], ...]
+    """
     url = f"http://airfoiltools.com/airfoil/seligdatfile?airfoil={airfoil_name}"
     response = requests.get(url)
     data = BeautifulSoup(response.text, "html.parser").text.splitlines()[1:]
@@ -27,6 +53,13 @@ def get_coords(airfoil_name):
 
 
 def get_all_airfoils():
+    """
+    Retrieves a list of all airfoils from the airfoiltools.com website.
+
+    Returns:
+        airfoils (list): A list of airfoil names.
+
+    """
     url = "http://airfoiltools.com/search/airfoils"
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser") \
@@ -62,8 +95,15 @@ def get_data(reynolds):
 
 
 if __name__ == "__main__":
+
     DATA_PATH = Path(__file__).parents[1] / "data"
+    
+    # Iterate over different Reynolds numbers
     for reynolds in [50_000, 100_000, 200_000, 500_000, 1_000_000]:
+
+        # Get airfoil data for the current Reynolds number
         json_data = get_data(reynolds)
+
+        # Save the data as JSON file
         with open(DATA_PATH / f"airfoil_data_{reynolds}.json", "w") as f:
-            json.dump(json_data, f, indent = 4)
+            json.dump(json_data, f, indent=4)
--- mlfoil/table_builder.py
+++ mlfoil/table_builder.py
@@ -3,51 +3,70 @@ from pathlib import Path
 import pandas as pd
 import json
 import numpy as np
-import aerosandbox as asb
-
 
 
 DATA_PATH = Path(__file__).parents[1] / "data"
 
 
-def calculate_CST_parameters(name, x, y):
-    coordinate_airfoil = asb.Airfoil(name, np.stack((x, y), axis=1))
-    kulfan_airfoil = coordinate_airfoil.to_kulfan_airfoil()
-    params = kulfan_airfoil.kulfan_parameters
-    for key, value in params.items():
-        if type(value) == np.ndarray:
-            params[key] = value.tolist()
-        if type(value) == np.float64:
-            params[key] = float(value)
-    return params
+def json_ingestion(file: Path):
+    """
+    Ingests a JSON file and returns the Reynolds number and the data.
 
+    Parameters:
+    file (Path): The path to the JSON file.
 
-def json_extraction(file: Path):
+    Returns:
+    tuple: A tuple containing the Reynolds number (extracted from the file name) and the loaded data from the JSON file.
+    """
     print(f"Processing {file.stem}")
     reynolds = file.stem.split('_')[-1]
     data = json.load(open(file, 'r'))
     return reynolds, data
 
 
-def build_table(json_files: list[Path]):
-    df = pd.DataFrame()
+def safe_get(data: dict, dot_chained_keys: str):
+    """
+    Safely retrieves a value from a nested dictionary using dot-chained keys.
 
+    Args:
+        data (dict): The nested dictionary to retrieve the value from.
+        dot_chained_keys (str): The dot-chained keys representing the path to the desired value.
+
+    Returns:
+        The value retrieved from the nested dictionary, or None if any of the keys are not found.
+
+    """
+    keys = dot_chained_keys.split(".")
+    for key in keys:
+        try:
+            if isinstance(data, list):
+                data = data[0][key]
+            else:
+                data = data[key]
+        except (KeyError, TypeError, IndexError):
+            return None
+    return data
+
+
+def build_table(json_files: list[Path]):
+    """
+    Builds a table from a list of JSON files.
+
+    Args:
+        json_files (list[Path]): A list of JSON file paths.
+
+    Returns:
+        pd.DataFrame: The resulting table containing data from the JSON files.
+    """
+    df = pd.DataFrame()
     for file in json_files:
-        reynolds, data = json_extraction(file)
+        reynolds, airfoil_data = json_ingestion(file)
         rows = []
 
-        for airfoil_name, airfoil_data in data.items():
-            angles = airfoil_data['polars']['alpha']
-            cd_values = airfoil_data['polars']['cd']
-            cl_values = airfoil_data['polars']['cl']
-            cm_values = airfoil_data['polars']['cm']
-            x = airfoil_data['coords']['x']
-            y = airfoil_data['coords']['y']
-
-            params = calculate_CST_parameters(airfoil_name, x, y)
-
-            angles_array = np.full(len(angles))
-            combinations = np.column_stack((angles_array, reynolds), angles)
+        for airfoil_name, data in airfoil_data.items():
+            angles = safe_get(data, 'polars.alpha')
+            reynolds_array = np.full(len(angles), reynolds)
+            combinations = np.column_stack((reynolds_array, angles))
 
             for i, (reynolds, angle) in enumerate(combinations):
                 rows.append({
@@ -55,19 +74,14 @@ def build_table(json_files: list[Path]):
                     'image': str(DATA_PATH / 'jpg' / f'{airfoil_name}.jpg'),
                     'angle': angle,
                     'reynolds': reynolds,
-                    'x_coords': ' '.join(map(str, x)),
-                    'y_coords': ' '.join(map(str, y)),
-                    'cst_upper_weights': ' '.join(map(str, params['upper_weights'])),
-                    'cst_lower_weights': ' '.join(map(str, params['lower_weights'])),
-                    'cst_leading_edge_weight': params['leading_edge_weight'],
-                    'cst_trailing_edge_thickness': params['TE_thickness'],
-                    'cd': cd_values[i],
-                    'cl': cl_values[i],
-                    'cm': cm_values[i]
+                    'x_coords': ' '.join(map(str, safe_get(data, 'coords.x'))),
+                    'y_coords': ' '.join(map(str, safe_get(data, 'coords.y'))),
+                    'cd': safe_get(data, 'polars.cd')[i],
+                    'cl': safe_get(data, 'polars.cl')[i],
+                    'cm': safe_get(data, 'polars.cm')[i]
                 })
         
         df = pd.concat([df, pd.DataFrame(rows)], ignore_index=True)
-
     return df
 
 
@@ -75,20 +89,7 @@ if __name__ == '__main__':
 
     json_files = [file for file in DATA_PATH.glob('json/*.json')]
     df = build_table(json_files)
-    df = df[~df['name'].isin([
-        'fx71l150',
-        'rae69ck',
-        'hq259b',
-        'naca642215',
-        'goe525',
-        'nacam3',
-        'goe801'
-    ])]
-
-    df.to_csv(DATA_PATH / 'csv' / 'airfoil_data.csv', index=False)
-    df[df.columns.drop('image')].to_csv(
-        DATA_PATH / 'csv' / 'DeepLearWing.csv', index=False
-    )
+    df.to_csv(DATA_PATH / 'csv' / 'data.csv', index=False)
 
 
 
