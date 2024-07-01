80d2fe64b3db4078d1e01a058d0db34abb321e10
victorien <victorienmichel3@gmail.com>
2024-07-01 14:19:15 +0200

    FIX: change array data type

--- deeplearwing/src/plot.py
+++ deeplearwing/src/plot.py
@@ -15,9 +15,6 @@ else:
 
 DATA_PATH = Path(__file__).parents[2] / "data"
 
-import numpy as np
-from PIL import Image, ImageDraw
-from typing import List
 
 def airfoil_to_image(x: List[float], y: List[float], image_width: int = 512, image_height: int = 128) -> np.ndarray:
     """
@@ -36,7 +33,7 @@ def airfoil_to_image(x: List[float], y: List[float], image_width: int = 512, ima
     normalized_coords = normalize_coordinates(x, y, image_width, image_height)
     img = create_white_image(image_width, image_height)
     draw_airfoil(img, normalized_coords)
-    return np.array(img).astype(float)
+    return np.asarray(img).astype('float32')
 
 
 def normalize_coordinates(x: List[float], y: List[float], image_width: int, image_height: int) -> List[Tuple[int, int]]:
