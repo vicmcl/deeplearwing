5e66302a5b265975541e0ebe4e16a19991ad015e
victorien <victorienmichel3@gmail.com>
2024-07-04 12:09:14 +0200

    Update dependencies

--- deeplearwing/src/__init__.py
+++ deeplearwing/src/__init__.py
@@ -1,5 +1,8 @@
 from .features import *
 from .plot import *
 from .scrap import *
-from .table_builder import * 
-from .checkpoint import *
+from .table_builder import *
+from .curvature_heatmap import *
+
+if "google.colab" in sys.modules:
+    from .checkpoint import *
